caf478caf9a1443b472622f710596d0f61be4cec
QuentinM22 <quentin.meunier1@laposte.net>
2024-04-01 17:42:21 +0200

    fetch

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,7 @@
 export const fetchData = async () => {
+    console.log(window.location.href);
     try {
-        const res = await fetch('/data/data.json');
+        const res = await fetch(`./data/data.json` );
         if (!res.ok) {
             throw new Error('La réponse du réseau n\'est pas valide');
         }
