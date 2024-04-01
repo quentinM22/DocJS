1fe248056f408f560b3fffcf707b7694bee4f089
QuentinM22 <quentin.meunier1@laposte.net>
2024-04-01 17:51:06 +0200

    fetch

--- js/fetch.js
+++ js/fetch.js
@@ -2,6 +2,7 @@ export const fetchData = async () => {
 const url = 'https://quentinm22.github.io/DocJS/data/data.json'
     try {
         const res = await fetch(url);
+        console.log(res);
         if (!res.ok) {
             throw new Error('La réponse du réseau n\'est pas valide');
         }
